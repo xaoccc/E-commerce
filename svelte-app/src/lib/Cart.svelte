43405c12e6ecc2a44f0f507fe4859bfd2c0d6798
xaoccc <xaocccc@gmail.com>
2025-11-07 15:19:12 +0200

    add remove cart

--- svelte-app/src/lib/Cart.svelte
+++ svelte-app/src/lib/Cart.svelte
@@ -1,26 +1,33 @@
 <script lang="ts">
-    let {cart, products} = $props();
-
+    let { cart, products } = $props();
+    const cartIds = Object.keys(cart);
 </script>
+
 <div class="cart">
     <p>Cart</p>
-    <hr>
-    {#if Object.keys(cart).length != 0}
-     {#each Object.keys(cart) as product}
-        <div class="product-data-container row g1">
-            <img src={products[product].smallImages[0]} alt="">
-            <div>
-                <p>{products[product].title}</p>
-                <p>${(products[product].old_price * products[product].discount).toFixed(2)} x 
-                    {products[product].qty} <span class="bold">${(products[product].old_price * products[product].discount) * products[product].qty}</span> </p>
-
+    <hr />
+    {#if cartIds.length != 0}
+        {#each cartIds as product}
+            <div class="product-data-container row g1">
+                <img src={products[product].smallImages[0]} alt="" />
+                <div>
+                    <p>{products[product].title}</p>
+                    <p>
+                        ${(
+                            products[product].old_price *
+                            products[product].discount
+                        ).toFixed(2)} x
+                        {products[product].qty}
+                        <span class="bold"
+                            >${products[product].old_price *
+                                products[product].discount *
+                                products[product].qty}</span
+                        >
+                    </p>
+                </div>
             </div>
-            
-        </div>
-
-     {/each}
+        {/each}
     {:else}
-     <p>Your cart is empty</p>
+        <p>Your cart is empty</p>
     {/if}
-
 </div>
