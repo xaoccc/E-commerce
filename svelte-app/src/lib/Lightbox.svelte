ddedc39343f927af9d91f875e8e5c2f6b1c51d19
xaoccc <xaocccc@gmail.com>
2025-11-13 15:02:40 +0200

    buttons previous and next item

--- svelte-app/src/lib/Lightbox.svelte
+++ svelte-app/src/lib/Lightbox.svelte
@@ -1,20 +1,42 @@
 <script lang="ts">
     let { showLightBox = $bindable(), currentProduct } = $props();
     let imgNum = $state(0);
+    const lastImgIdx = currentProduct.smallImages.length - 1
+    function showPrev() {
+        (imgNum == 0) ? imgNum = lastImgIdx : imgNum -= 1;
+    }
+    function showNext() {
+        (imgNum == lastImgIdx) ? imgNum = 0 : imgNum += 1;
+    }
 </script>
 
 <section class="lightbox">
-    
-    <section class="g2">
-        <button onclick={() => (showLightBox = !showLightBox)} aria-label="close button"></button>
-        <input
-            type="image"
-            src={currentProduct.bigImages[imgNum]}
-            alt={`${currentProduct.title} Big Image`}
-            name="saveForm"
-            class="main-img"
-            id="saveForm"
-        />
+    <section class="g1">
+        <button
+            onclick={() => (showLightBox = !showLightBox)}
+            aria-label="close button"
+        ></button>
+        <div class="row main-img-container">
+            <button
+                onclick={() => showPrev()}
+                aria-label="previous"
+                class="btn-prev"
+            ></button>
+            <input
+                type="image"
+                src={currentProduct.bigImages[imgNum]}
+                alt={`${currentProduct.title} Big Image`}
+                name="saveForm"
+                class="main-img"
+                id="saveForm"
+            />
+            <button
+                onclick={() => showNext()}
+                aria-label="next"
+                class="btn-next"
+            ></button>
+        </div>
+
         <div class="row g2 small-img-container">
             {#each currentProduct.smallImages as smallImg, i}
                 <img
